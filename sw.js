4d9c5ad3e8f34d4455973e99ded5a28b079112b3
Aaron <kammingwai@Aarons-MacBook-Air.local>
2026-04-23 16:27:31 +0800

    Bump app version to 2026.04.23.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teochew-v2026.04.14.4';
+const CACHE = 'teochew-v2026.04.23.1';
 
 function shouldCache(request, response) {
   return request.method === 'GET' && response && response.ok;
